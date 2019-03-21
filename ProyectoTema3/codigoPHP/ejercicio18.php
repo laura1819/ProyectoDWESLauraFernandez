bcaaaed75a21fcb1c776c01be5d291f83abe195e
Laura Fernandez <laura.ferfer.7@educa.jcyl.es>
2019-03-21 10:08:10 +0100

    Mejoras ejercicio del 18 del Tema3

--- ProyectoTema3/codigoPHP/ejercicio18.php
+++ ProyectoTema3/codigoPHP/ejercicio18.php
@@ -19,36 +19,24 @@
           Comentarios: Recorrer el array anterior utilizando funciones para obtener el mismo resultado.
          */
         
-        
-
-
-
         $teatro[7][9] = "persona1"; // valores para la persona 1
         $teatro[13][7] = "persona2"; // valores para la persona 2
         $teatro[2][9] = "persona3"; // valores para la persona 3
         $teatro[11][12] = "persona4"; // valores para la persona 4
         $teatro[1][14] = "persona5"; // valores para la persona 5
-
-
-        while (list($clave, $valor) = each($teatro)) {  
-            echo "en la fila : " . $clave . " y en el asiento : " ;
-            while (list($clavedos, $valordos) = each($valor)){                
-              echo $clavedos . " se sienta : " .$valordos;  
-              echo "<br>";
-               //echo var_dump($valor);
+        
+        
+        while (list($fila, $aAsiento) = each($teatro)) {  // le pasamos a list dos parametros del array teatro
+            echo "en la fila : " . $fila . " y en el asiento : " ; // sacamos un mensaje con la fila
+            while (list($asiento, $persona) = each($aAsiento)){  // le pasamos a list dos parametros del array aAsiento               
+              echo $asiento . " se sienta : " .$persona;  // sacamos un mensaje con el asiento y la persona
+              echo "<br>"; // hacemos un salto de linea para que salgan separados
+              //echo var_dump($valor);
               //echo var_dump($clave);
               //echo var_dump($valordos);
               //echo var_dump($clavedos);
-            }
-            
+            }   
         }
-        
-        
-        
-       
-         
-        
-        
         ?>                                 
 
     </body>
